06d6aaa06d83371b1c7f46a34cdcfb1ef897704b
giadavolpin <volpingiada@gmail.com>
2022-10-24 14:32:01 +0200

    modifiche

--- script.js
+++ script.js
@@ -5,23 +5,24 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 
 //chiedo di inserire una parola
-let parolaUtente = document.getElementById('parolaUtente');
-let palindroma = document.getElementById('palindroma');
+let parolaUtente = document.getElementById('parolaUtente').value;
+let button = document.querySelector('palindroma');
 
 // funzione che inizia con un evento
 palindroma.addEventListener('click', function(){ 
-let palindroma = parolaUtente.value.split('').reverse().join('');
-/* console.log(palindroma); */
-} ) 
-
+let parola = parolaUtente.value.split('').reverse().join(''); 
+console.log(parola);
 //stampa se è vero o falso
 if (parolaUtente == palindroma){
         console.log('la parola è palindroma')
 } else {
         console.log('la parola non è palindroma')
-  }   
+  }  
 
-  /* NON FUNZIONA MI SA SEMPRE CHE NON è PALINDROMA */
+} ) 
+
+
+ console.log(parola)
 
 
 
